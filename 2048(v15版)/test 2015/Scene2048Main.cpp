42076ac4450bd26caaf7ce1b687d984b5d8896a1
Shigeoji <h1201919018@hiratagakuen.onmicrosoft.com>
2020-11-17 11:41:54 +0900

    Initial commit

---
+++ 2048(v15版)/test 2015/Scene2048Main.cpp
@@ -0,0 +1,48 @@
+//STLデバッグ機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawFont.h"
+#include "GameL\Audio.h"
+//#include "GameL\DrawTexture.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用するヘッダー
+#include"Scene2048Main.h"
+#include"GameHead.h"
+
+//コンストラクタ
+CScene2048Main::CScene2048Main()
+{
+
+}
+
+//デストラクタ
+CScene2048Main::~CScene2048Main()
+{
+
+}
+
+//ゲームメイン初期化メソッド
+void CScene2048Main::InitScene()
+{
+	CObj2048Main* p = new CObj2048Main();
+	Objs::InsertObj(p, OBJ_2048, 1);
+
+	//グラフィック読み込み
+
+	//音楽情報読み込み
+
+	//SE情報の登録
+
+}
+
+//ゲームメイン実行メソッド
+void CScene2048Main::Scene()
+{
+
+}
